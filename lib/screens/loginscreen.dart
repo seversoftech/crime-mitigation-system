8ca8c4dd033b407e90d611d9eaf31f0482f618bd
IBEABUCHI EMMANUEL IHEKE (Sever) <developersever@gmail.com>
2023-05-29 16:54:04 +0100

    ...

--- lib/screens/loginscreen.dart
+++ lib/screens/loginscreen.dart
@@ -9,55 +9,53 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Image.asset(
-                'assets/icons/icon.png',
-                height: 150,
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset(
+              'assets/icons/icon.png',
+              height: 150,
+            ),
+            const SizedBox(height: 20.0),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12))),
               ),
-              const SizedBox(height: 20.0),
-              const TextField(
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(12))),
+            ),
+            const SizedBox(height: 10.0),
+            const TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12))),
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            ElevatedClickButton(
+              onPressed: () {},
+              child: const Text('Login'),
+            ),
+            const SizedBox(height: 4.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextClickButton(
+                  onPressed: () {},
+                  text: 'Forget Password',
                 ),
-              ),
-              const SizedBox(height: 10.0),
-              const TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(12))),
-                ),
-              ),
-              const SizedBox(height: 20.0),
-              ElevatedClickButton(
-                onPressed: () {},
-                child: const Text('Login'),
-              ),
-              const SizedBox(height: 4.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  TextClickButton(
-                    onPressed: () {},
-                    text: 'Forget Password',
-                  ),
-                  TextClickButton(
-                    onPressed: () {},
-                    text: 'Sign Up',
-                  )
-                ],
-              ),
-            ],
-          ),
+                TextClickButton(
+                  onPressed: () {},
+                  text: 'Sign Up',
+                )
+              ],
+            ),
+          ],
         ),
       ),
     );
